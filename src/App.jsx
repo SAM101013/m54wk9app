5180a6dcfb02aa1d29c76962d013ac9babbf466c
SAM101013 <amoonmarketing@proton.me>
2024-02-22 11:21:22 +0000

    updated components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import LogOrSign from "./components/logorsign/LogOrSign";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <LogOrSign />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/logorsign/LogOrSign.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Login from "./components/login/login";
+import Signup from "./components/signup/Signup";
+
+import "LogOrSign.css";
+
+const LogOrSign = () => {
+  return;
+  <div className="logorsign-wrapper">
+    <login />
+    <Signup />
+  </div>;
+};
+
+export default LogOrSign;
